7885c277176f4a438b3310f1a05959a5c480e4e7
Will Luna <willluna.zac.com@gmail.com>
2023-10-03 13:55:28 -0500

    first commit (0-10(Class of course platzi the add button))

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <Suspense>
+    <template #default>
+      <Home />
+    </template>
+    <template #fallback>
+      <SplashScreen />
+    </template>
+  </Suspense>
+</template>
+
+<script>
+import SplashScreen from "./components/SplashScreen";
+import { defineAsyncComponent } from "vue";
+
+export default {
+  name: "App",
+  components: {
+    SplashScreen,
+    Home: defineAsyncComponent(
+      () =>
+        new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(import("./components/Home.vue"));
+          }, 2500);
+        })
+    ),
+  },
+};
+</script>
+
+<style>
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,114 @@
+<template>
+  <Layout>
+    <template #header>
+      <Header></Header>
+    </template>
+    <template #resume>
+      <Resume
+        :total-label="'Ahorro total'"
+        :label="label"
+        :total-amount="100000"
+        :amount="amount"
+      >
+        <template #graphic> Graphic </template>
+        <template #action> action </template>
+      </Resume>
+    </template>
+    <template #movements>
+      <Movements :movements="movements" />
+    </template>
+  </Layout>
+</template>
+
+<script>
+import Layout from "./Layout.vue";
+import Header from "./Header.vue";
+import Resume from "./Resume/Index.vue";
+import Movements from "./Movements/Index.vue";
+
+export default {
+  components: {
+    Layout,
+    Header,
+    Resume,
+    Movements,
+  },
+  data() {
+    return {
+      label: null,
+      amount: null,
+      movements: [
+        {
+          id: 0,
+          title: "Movimiento 1",
+          description: "Lorem ipsum dolor sit amet",
+          amount: 1000,
+        },
+        {
+          id: 1,
+          title: "Movimiento 2",
+          description: "Lorem ipsum dolor sit amet",
+          amount: 1000,
+        },
+        {
+          id: 2,
+          title: "Movimiento 3",
+          description: "Lorem ipsum dolor sit amet",
+          amount: -11000,
+        },
+        {
+          id: 3,
+          title: "Movimiento 4",
+          description: "Lorem ipsum dolor sit amet",
+          amount: -1000,
+        },
+        {
+          id: 4,
+          title: "Movimiento 5",
+          description: "Lorem ipsum dolor sit amet",
+          amount: -5000,
+        },
+        {
+          id: 5,
+          title: "Movimiento 6",
+          description: "Lorem ipsum dolor sit amet",
+          amount: 1000,
+        },
+        {
+          id: 6,
+          title: "Movimiento 7",
+          description: "Lorem ipsum dolor sit amet",
+          amount: 1000,
+        },
+        {
+          id: 7,
+          title: "Movimiento 8",
+          description: "Lorem ipsum dolor sit amet",
+          amount: 1000,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.movements {
+  max-height: 100%;
+  padding: 0 8px;
+  margin-bottom: 14px;
+}
+
+.title {
+  margin: 8px 16px 24px 16px;
+  color: var(--brand-blue);
+}
+
+.content {
+  max-height: 68vh;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  overflow-y: scroll;
+}
+</style>
