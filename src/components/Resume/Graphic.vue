4ba244e7a14e8e77ad565aba6753f625c1bfde5f
Will Luna <willluna.zac.com@gmail.com>
2023-10-16 14:21:36 -0500

    Creacion de la logica del grafico

--- src/components/Resume/Graphic.vue
+++ src/components/Resume/Graphic.vue
@@ -5,9 +5,9 @@
         stroke="#c4c4c4"
         stroke-width="2"
         x1="0"
-        y1="100"
+        :y1="zero"
         x2="300"
-        y2="100"
+        :y2="zero"
       />
       <polyline
         fill="none"
@@ -25,6 +25,7 @@
       />
     </svg>
     <p>Last 30 days</p>
+    <div>{{ zero }}</div>
   </div>
 </template>
 
@@ -40,25 +41,29 @@ const props = defineProps({
 
 const { amounts } = toRefs(props);
 
-const amountToPixels = () => {
+const amountToPixels = (amount) => {
   const min = Math.min(...amounts.value);
   const max = Math.max(...amounts.value);
 
-  return `${min}, ${max}`;
+  const amountAbs = amount + Math.abs(min);
+  const minmax = Math.abs(max) + Math.abs(min);
+
+  return 200 - ((amountAbs * 100) / minmax) * 2;
 };
+
+const zero = computed(() => {
+  return amountToPixels(0);
+});
 
 const points = computed(() => {
   const total = amounts.value.length;
 
-  return Array(total)
-    .fill(100)
-    .reduce((points, amount, i) => {
-      const x = (300 / total) * (i + 1);
-      const y = amountToPixels(amount);
-      console.log(y);
-
-      return `${points} ${x}, ${y}`;
-    }, "0, 100");
+  return amounts.value.reduce((points, amount, i) => {
+    const x = (300 / total) * (i + 1);
+    const y = amountToPixels(amount);
+    console.log(y);
+    return `${points} ${x}, ${y}`;
+  }, "0, 100");
 });
 </script>
 
