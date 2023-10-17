242ec11bc4216062f71b6df00be5b2644757d745
Will Luna <willluna.zac.com@gmail.com>
2023-10-17 13:59:00 -0500

    16- modelo de dato

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -49,52 +49,94 @@ export default {
           id: 0,
           title: "Movimiento 1",
           description: "Lorem ipsum dolor sit amet",
-          amount: 1000,
+          amount: 100,
+          time: new Date("02-01-2022"),
         },
         {
           id: 1,
           title: "Movimiento 2",
           description: "Lorem ipsum dolor sit amet",
-          amount: 1000,
+          amount: 200,
+          time: new Date("02-01-2022"),
         },
         {
           id: 2,
           title: "Movimiento 3",
           description: "Lorem ipsum dolor sit amet",
-          amount: -11000,
+          amount: 500,
+          time: new Date("02-01-2022"),
         },
         {
           id: 3,
           title: "Movimiento 4",
           description: "Lorem ipsum dolor sit amet",
-          amount: -1000,
+          amount: 200,
+          time: new Date("02-01-2022"),
         },
         {
           id: 4,
           title: "Movimiento 5",
           description: "Lorem ipsum dolor sit amet",
-          amount: -5000,
+          amount: -400,
+          time: new Date("02-01-2022"),
         },
         {
           id: 5,
           title: "Movimiento 6",
           description: "Lorem ipsum dolor sit amet",
-          amount: 1000,
+          amount: -600,
+          time: new Date("02-01-2022"),
         },
         {
           id: 6,
           title: "Movimiento 7",
           description: "Lorem ipsum dolor sit amet",
-          amount: 1000,
+          amount: -300,
+          time: new Date("02-01-2022"),
         },
         {
           id: 7,
           title: "Movimiento 8",
           description: "Lorem ipsum dolor sit amet",
-          amount: 1000,
+          amount: 0,
+          time: new Date("02-01-2022"),
+        },
+        {
+          id: 8,
+          title: "Movimiento 9",
+          description: "Lorem ipsum dolor sit amet",
+          amount: 300,
+          time: new Date("01-01-2022"),
+        },
+        {
+          id: 9,
+          title: "Movimiento 10",
+          description: "Lorem ipsum dolor sit amet",
+          amount: 500,
+          time: new Date("01-01-2022"),
         },
       ],
     };
+  },
+  computed: {
+    amounts() {
+      const lastDays = this.movements
+        .filter((m) => {
+          const today = new Date();
+          const oldDate = today.setDate(today.getDate() - 30);
+
+          return m.time > oldDate;
+        })
+        .map((m) => m.amount);
+
+      return lastDays.map((m, i) => {
+        const lastMovements = lastDays.slice(0, i);
+
+        return lastMovements.reduce((suma, movement) => {
+          return suma + movement;
+        }, 0);
+      });
+    },
   },
 };
 </script>
--- src/components/Resume/Graphic.vue
+++ src/components/Resume/Graphic.vue
@@ -31,12 +31,11 @@
       />
     </svg>
     <p>Last 30 days</p>
-    <div>{{ zero }}</div>
   </div>
 </template>
 
 <script setup>
-import { defineProps, toRefs, computed, ref } from "vue";
+import { ref, toRefs, defineProps, defineEmits, computed } from "vue";
 
 const props = defineProps({
   amounts: {
@@ -74,12 +73,15 @@ const points = computed(() => {
 const showPointer = ref(false);
 const pointer = ref(0);
 
+const emit = defineEmits(["Select"]);
+
 const tap = ({ target, touches }) => {
   showPointer.value = true;
   const elementWidth = target.getBoundingClientRect().width;
   const elementX = target.getBoundingClientRect().x;
   const touchX = touches[0].clientX;
   pointer.value = ((touchX - elementX) * 300) / elementWidth;
+  emit("Select");
 };
 
 const untap = () => {
